aca50fa938cad32ea04c67fa7ccd2402757cb8d2
Dawn McKnight <demcknig@ualberta.ca>
2023-11-14 03:50:26 -0700

    Updating readthedocs site (adding theme and references to code, as well as adding custom css.

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,13 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../bytemaker'))
+sys.path.insert(0, os.path.abspath('../bytemaker/native_types'))
+
+
 project = 'bytemaker'
 copyright = '2023, DEMcKnight'
 author = 'DEMcKnight'
@@ -13,15 +20,25 @@ author = 'DEMcKnight'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.viewcode',
+    'sphinxcontrib.napoleon'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_css_files = [
+    'custom.css',
+]
+html_theme = 'piccolo_theme'
 html_static_path = ['_static']
+
+
+html_css_files = [
+    'css/custom.css',
+]
--- docs/index.rst
+++ docs/index.rst
@@ -3,13 +3,14 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to bytemaker's documentation!
+Welcome to bytemaker!
 =====================================
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 3
    :caption: Contents:
 
+   source/modules
 
 
 Indices and tables
