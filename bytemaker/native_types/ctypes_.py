b392501c792b1882af663ae06f4a267813ad0b60
Dawn McKnight <demcknig@ualberta.ca>
2024-04-21 02:54:20 -0600

    Having ctypes_.py comply with PEP8

--- bytemaker/native_types/ctypes_.py
+++ bytemaker/native_types/ctypes_.py
@@ -45,17 +45,20 @@ def ctype_to_bytes(ctype_obj: CType, reverse_endianness=True) -> bytes:
     Function to convert ctypes into bytes objects
 
     Args:
-        ctype_obj (ctypes._SimpleCData | ctypes.Structure | ctypes.Union | ctypes.Array):
+        ctype_obj (ctypes._SimpleCData | ctypes.Structure |
+                ctypes.Union | ctypes.Array):
             The ctypes object to convert to bytes
         reverse_endianness (bool, optional):
-            Whether to reverse the endianness of the bytes after converting. Defaults to False.
+            Whether to reverse the endianness of the bytes after
+                converting. Defaults to False.
 
     Returns:
         bytes: The bytes representation of the ctypes object
     """
     if not is_instance_of_union(ctype_obj, CType):
         raise TypeError(
-            f"ctype_to_bytes only accepts _SimpleCData, Structure, Union, and Array objects, not {type(ctype_obj)}"
+            f"ctype_to_bytes only accepts _SimpleCData, Structure,"
+            f"Union, and Array objects, not {type(ctype_obj)}."
         )
 
     if reverse_endianness:
@@ -71,15 +74,20 @@ def ctype_to_bits(ctype_obj: CType, reverse_endianness=True) -> Bits:
     Function to convert ctypes into Bits objects
 
     Args:
-        ctype_obj (ctypes._SimpleCData | ctypes.Structure | ctypes.Union | ctypes.Array):
+        ctype_obj (ctypes._SimpleCData | ctypes.Structure\
+                | ctypes.Union | ctypes.Array):
             The ctypes object to convert to Bits
         reverse_endianness (bool, optional):
-            Whether to reverse the endianness of the Bits after converting. Defaults to False.
+            Whether to reverse the endianness of the Bits\
+                after converting. Defaults to False.
 
     Returns:
         Bits: The Bits representation of the ctypes object
     """
-    return Bits(ctype_to_bytes(ctype_obj, reverse_endianness=reverse_endianness))
+    return Bits(ctype_to_bytes(
+        ctype_obj,
+        reverse_endianness=reverse_endianness)
+    )
 
 
 def bytes_to_ctype(
@@ -92,17 +100,19 @@ def bytes_to_ctype(
         bytes_obj (bytes): The bytes object to convert to a ctypes object
         ctype_type (type): The type of the ctypes object to convert to.
             Must be a member of CType
-        reverse_endianness (bool, optional): Whether to reverse the endianness
-            of the bytes before converting. Defaults to False.
+        reverse_endianness (bool, optional): Whether to reverse the
+            endianness of the bytes before converting. Defaults to False.
 
     Returns:
-        ctypes._SimpleCData | ctypes.Structure | ctypes.Union | ctypes.Array:
+        ctypes._SimpleCData | ctypes.Structure | ctypes.Union
+            | ctypes.Array:
             The ctypes object representation of the bytes
     """
 
     if not is_subclass_of_union(ctype_type, CType):
         raise TypeError(
-            f"bytes_to_ctype only accepts _SimpleCData, Structure, Union, and Array types, not {ctype_type}"
+            f"bytes_to_ctype only accepts _SimpleCData, Structure,"
+            f"Union, and Array types, not {ctype_type}."
         )
 
     ctype_obj = ctype_type.from_buffer_copy(bytes_obj)
@@ -120,13 +130,16 @@ def bits_to_ctype(bits_obj: Bits, ctype_type: type, reverse_endianness=True) -> 
         bits_obj (Bits): The bits object to convert to a ctypes object
         ctype_type (type): The type of the ctypes object to convert to.
             Must be a member of CType
-        reverse_endianness (bool, optional): Whether to reverse the endianness
-            of the bits before converting. Defaults to False.
+        reverse_endianness (bool, optional): Whether to reverse the
+            endianness of the bits before converting. Defaults to False.
 
     Returns:
-        ctypes._SimpleCData | ctypes.Structure | ctypes.Union | ctypes.Array:
+        ctypes._SimpleCData | ctypes.Structure | ctypes.Union
+            | ctypes.Array:
             The ctypes object representation of the bits
     """
     return bytes_to_ctype(
-        bits_obj.to_bytes(), ctype_type, reverse_endianness=reverse_endianness
+        bits_obj.to_bytes(),
+        ctype_type,
+        reverse_endianness=reverse_endianness
     )
