32b4c2235f34f84b8dab20c8ee4ffd8c58051d07
Dawn McKnight <demcknig@ualberta.ca>
2024-08-16 14:09:23 -0600

    Adding BitsConstructible type to BitArray symbols

--- bytemaker/BitArray.py
+++ bytemaker/BitArray.py
@@ -1,8 +1,12 @@
 from importlib.util import find_spec
 
 if find_spec("bitarray"):
-    from bytemaker.BitArray_with_bitarray_speedup import BitArray, BitsCastable
+    from bytemaker.BitArray_with_bitarray_speedup import (
+        BitArray,
+        BitsCastable,
+        BitsConstructible,
+    )
 else:
     raise NotImplementedError("TODO")
 
-__all__ = ["BitArray", "BitsCastable"]
+__all__ = ["BitArray", "BitsCastable", "BitsConstructible"]
