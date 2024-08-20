ce853df4e8ab041df0ed3846925abc9a2040574c
Dawn McKnight <demcknig@ualberta.ca>
2024-08-20 13:18:20 -0600

    Defaulting to _Buffer for specialized factory-created Buffer subclasses (barring a name given as an argument). This is a change from previous behavior, where it was Buffer{num_bits}

--- bytemaker/bittypes/buffer.py
+++ bytemaker/bittypes/buffer.py
@@ -19,6 +19,29 @@ class Buffer(BitType[BitVector]):
 
     Use the `specialize` method to create a subclass with the desired number of bits
         or use one of the pre-defined subclasses.
+
+    Class Attributes:
+    -----------------
+    num_bits : int
+        The number of bits in instances of this `Buffer` subclass.
+    base_bit_type : Type[Buffer]
+        The base `BitType` this class derives from. It will be `Buffer`.
+    py_type : Type[BitVector]
+        The type that this `BitType` represents. It is `BitVector`.
+
+    Instance Attributes
+    -------------------
+    bits : BitVector
+       The underlying sequence of bits of this `Buffer` object. Identical to `value`.
+    value : BitVector
+       The `BitVector` value of this `Buffer` object. Identical to `bits`.
+    """
+
+    """
+    A BitType that represents a buffer of bits.
+
+    Use the `specialize` method to create a subclass with the desired number of bits
+        or use one of the pre-defined subclasses.
     """
 
     py_type = BitVector
@@ -35,6 +58,14 @@ class Buffer(BitType[BitVector]):
     def specialize(cls: Type[BufferSelf], num_bits_: int, name_: Optional[str] = None):
         """
         Returns a subclass of Buffer with the specified number of bits.
+
+        Args:
+            num_bits_ (int): The number of bits the buffer should have.
+            name_ (Optional[str], optional): The name of the subclass. Defaults to None,
+                meaning the name will be _Buffer.
+
+        Returns:
+            Type[BufferSelf]: A subclass of Buffer with the specified number of bits.
         """
 
         class _Buffer(cls):
@@ -42,11 +73,6 @@ class Buffer(BitType[BitVector]):
 
         if name_:
             _Buffer.__name__ = name_
-        else:
-            _Buffer.__name__ = f"Buffer{num_bits_}"
-
-        # _Buffer_Genericized: Type[BufferSelf] = _Buffer[
-        #     Literal[num_bits_]]  # type: ignore[reportUndefinedVariable]
 
         return _Buffer
 
